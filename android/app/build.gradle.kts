803a75d05ec0c18c150491951c81bb79aa4f7a59
antonio venancio <antoniovenancio10@gmail.com>
2025-05-04 17:45:41 -0300

    ajsutes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,12 +21,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     signingConfigs {
@@ -55,7 +55,8 @@ android {
 dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
     implementation("androidx.multidex:multidex:2.0.1")
 
     // Solução 1: Usar versão estável recomendada do WebRTC SDK
